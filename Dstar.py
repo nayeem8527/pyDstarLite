e5f45c399094efcec1289803a70f97df0c7f220b
slofmb <loizoustephanos@gmail.com>
2017-01-27 16:21:43 +0000

    vis

--- Dstar.py
+++ Dstar.py
@@ -1,7 +1,8 @@
 import Queue
 import math
 import copy
-
+import cv2
+import numpy as np
 class state:
 	x= 0
 	y= 0
@@ -54,6 +55,8 @@ def eightCondist(a,b):
 
 
 class Dstar:
+	minx,miny,maxx,maxy=0,0,200,200
+	obstacles = []
 	openList = Queue.PriorityQueue()
 	cellHash = {}
 	openHash = {}
@@ -76,7 +79,12 @@ class Dstar:
 
 		tmp = cellInfo()
 		tmp.g = tmp.rhs = 0
-		tmp.cost = self.C1
+		tmp.cost = self.trueDist(self.s_start,self.s_goal)
+
+		self.cellHash[self.s_start] = tmp
+
+		self.s_start = self.calculateKey(self.s_start)
+		self.s_last = self.s_start
 
 
 
@@ -88,6 +96,10 @@ class Dstar:
 	def makeNewCell(self,u):
 		exists = self.cellHash.get(u,None)
 		if exists: return
+		self.maxx = max([self.maxx,u.x]) 
+		self.minx = min([self.minx,u.x]) 
+		self.maxy = max([self.maxy,u.y]) 
+		self.miny = min([self.miny,u.y]) 
 		cinf = cellInfo()
 		cinf.g = cinf.rhs = self.heuristic(u, self.s_goal)
 		cinf.cost = self.C1
@@ -138,6 +150,7 @@ class Dstar:
 
 		val = self.cellHash.get(a,None)
 		if val == None: return scale*self.C1
+		print "this SHOULD BE NEGATIVE",val.cost, val,a
 		return scale * val.cost
 
 	def updateCell(self,x,y,val):
@@ -151,23 +164,23 @@ class Dstar:
 		self.cellHash[u].cost = val
 		self.updateVertex(u)
 
-	def getSucc(self,u):
+	def getSucc(self,u,r = 1):
 		s = []
 		u.k[0], u.k[1] = -1, -1
 		if self.occupied(u): return
 
-		for x in range(-1,2):
+		for x in range(-r,1+r):
 			for y in range(-1,2):
-				print x,y
 				if not(x == 0 and y == 0):
 					s.append(state(u.x+x,u.y + y))
-		return s.reverse()
+		return s
 
 	def updateVertex(self,u):
 		s = []
 
 		if (u != self.s_goal):
 			succL = self.getSucc(u)
+			if not succL: succL = []
 			tmp = float("Inf")
 			tmp2 = None
 
@@ -205,7 +218,7 @@ class Dstar:
 		if cur == None: return
 		del self.openHash[u]
 
-	def trueDist(a,b):
+	def trueDist(self,a,b):
 		x = a.x - b.x
 		y = a.y - b.y
 		return math.sqrt(x*x + y*y)
@@ -217,7 +230,6 @@ class Dstar:
 
 		for x in range(-1,2):
 			for y in range(-1,2):
-				print x,y
 				if not(x == 0 and y == 0):
 					nu = state(u.x+x,u.y + y)
 					if not self.occupied(nu): s.append(nu)
@@ -258,13 +270,14 @@ class Dstar:
 
 		tmp = cellInfo()
 		tmp.g = tmp.rhs = 0
-		tmp.cost = self.C1
+		#originally cost = c1 also in init
+		tmp.cost = self.trueDist(self.s_start,self.s_goal)
 
-		self.cellHash[s_goal] = tmp
+		self.cellHash[self.s_goal] = tmp
 		self.s_start = self.calculateKey(self.s_start)
 
 		for item in toAdd:
-			updateCell(item[0].x,item[0].y, item[1])
+			self.updateCell(item[0].x,item[0].y, item[1])
 
 
 	def computeShortestPath(self):
@@ -275,31 +288,131 @@ class Dstar:
 		k = 0
 		s_start =self.calculateKey(self.s_start)
 		while( (not self.openList.empty()) and 
-				(self.openList.get() < s_start) or
-				(self.getRHS(s_start) != self.getG(s_start))
-				)
-			k += 1
+		 (self.openList.queue[0] < s_start) or
+		 (self.getRHS(s_start) != self.getG(s_start))):
+			k = k + 1
+			print len(self.openList),"QUEEE"
 			if k > self.maxSteps:
 				print "Reached max steps"
 				return -1
 
-		u = state()
+			u = state()
 
-		test = (self.getRHS(self.s_start) == self.getG(self.s_start))
+			test = (self.getRHS(self.s_start) == self.getG(self.s_start))
 
-		while True:
-			if self.openList.empty(): return 1
-			u = self.openList.get()
+			while True:
+				if self.openList.empty(): return 1
+				u = self.openList.get()
 
-			if self.isValid(u):
-				if (not(u < s_start) and (not test)): return 2
-				break
+				if self.isValid(u):
+					if (not(u < s_start) and (not test)): return 2
+					break
+
+			cur = self.openHash.get(u)
+
+			k_old =  u;
+
+			if (k_old < calculateKey(u)):
+				self.insert(u)
+			elif (self.getG(u) > self.getRHS(u)):
+				self.setG(u,getRHS(u))
+				s = self.getPred(u)
+				for i in s:
+					self.updateVertex(i)
+
+			else:
+				self.setG(u,float("Inf"))
+				s = self.getPred(u)
+				for i in s:
+					self.updateVertex(i)
+				self.updateVertex(u)
+		print "STEPS", k
+		return 0
+
+	def replan(self):
+		self.path = []
+		path = []
+		res = self.computeShortestPath()
+		print res
+
+		if res < 0:
+			print "NO PATH1"
+			return False
 
 
+		cur_state= self.s_start
 
+		if (math.isinf(self.getG(self.s_start))):
+			print "NO PATH2"
+			return False
 
-x = Dstar(0, 10, 40, 40)
-y = state()
-y.x , y.y = 1,2
-x.makeNewCell(y)
-m = x.getSucc(y)
+		while cur_state != self.s_goal:
+			path.append(cur_state)
+			self.updateStart(cur_state.x,cur_state.y)
+			# print cur_state
+			self.visualize(path)
+			s = self.getSucc(cur_state)
+			if not s:
+				print "NO PATH3"
+				return False
+
+			cmin = float("Inf")
+			tmin = None
+
+			smin = state()
+
+			for i,place in enumerate(s):
+				val = self.cost(cur_state,place)
+				val2 = self.trueDist(place,self.s_goal) + self.trueDist(cur_state,place)
+
+				val += self.getG(place)
+				print "p",i,place, "val", val,"True Dist", val2, "cmin", cmin ,"min t",tmin
+				print "here>goal", self.trueDist(place,self.s_goal) ,"start>here", self.trueDist(self.s_start,place)
+				print
+				if self.close(val,cmin):
+					if tmin > val2:
+						tmin = val2
+						cmin = val
+						smin = place
+					elif val < cmin:
+						tmin = val2
+						cmin = val
+						smin = place
+			print cur_state, smin
+			cur_state = smin
+		path.append(self.s_goal)
+		self.path = path
+		return True
+
+	def visualize(self,path=None):
+		frame = np.zeros((self.maxx+ 30,self.maxy+30,3),np.uint8)
+		if path:
+			for x in path:
+				frame[x.x,x.y] = (255,0,0)
+		for node,info in self.cellHash.iteritems():
+			r = 255 * info.cost * -1
+			frame[node.x,node.y] = (0,0,r)
+		for x in self.path:
+			frame[x.x,x.y] = (0,255,0)
+		frame[self.s_start.x,self.s_start.y] = (255,0,0)
+		cv2.imshow("path",frame)
+		cv2.waitKey()
+
+#usage ()
+xx = Dstar(0, 10, 100, 100)
+# for x in range(xx.maxx):
+# 	for y in range(xx.maxy):
+		# xx.updateCell(x,y,0)
+for y in range(50):
+	xx.updateCell(50,y,-10)
+	xx.updateCell(51,y,-10)
+	xx.updateCell(y,2*y+3,-1)
+	xx.updateCell(y,2*y+1,-1)
+	xx.updateCell(y,2*y+2,-1)
+		
+
+xx.visualize()
+# xx.updatseGoal(100,100)
+xx.replan()
+xx.visualize()
+xx.replan()
